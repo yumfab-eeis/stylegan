caf618fb8743d94eabf8edee91ff9d64d702f597
yuma <fableooon@gmail.com>
2019-03-08 17:39:54 +0900

    debug

--- dnnlib/tflib/network.py
+++ dnnlib/tflib/network.py
@@ -182,7 +182,7 @@ class Network:
         self.own_vars = OrderedDict((var.name[len(self.scope) + 1:].split(":")[0], var) for var in tf.global_variables(self.scope + "/"))
         self.vars = OrderedDict(self.own_vars)
         self.vars.update((comp.name + "/" + name, var) for comp in self.components.values() for name, var in comp.vars.items())
-        for name, var in self.vars.items() if var.trainable:
+        for name, var in self.vars.items():
             print (name)
             print (type(var))
         self.trainables = OrderedDict((name, var) for name, var in self.vars.items() if var.trainable)
