4de8dedb758507c66ccdf29efe712692d3666ffa
yuma <fableooon@gmail.com>
2019-03-09 02:59:34 +0900

    cond

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ if 1:
     tf_config     = {'rnd.np_random_seed': 1000}                                           # Options for tflib.init_tf().
 
     # Dataset.
-    desc += '-maico2kiku_spring';     dataset = EasyDict(tfrecord_dir='maico2kiku_spring')
+    desc += '-spring';     dataset = EasyDict(tfrecord_dir='spring')
     #desc += '-ffhq';     dataset = EasyDict(tfrecord_dir='ffhq');                 train.mirror_augment = True
     #desc += '-ffhq512';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=512); train.mirror_augment = True
     #desc += '-ffhq256';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=256); train.mirror_augment = True
