0c550c74edb6ccd23a2e0eb431d4b5fbc49c6990
yuma <fableooon@gmail.com>
2019-03-09 23:46:00 +0900

    add new file

---
+++ generate_video_frame.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+"""Minimal script for generating an image using pre-trained StyleGAN generator."""
+
+import os
+import pickle
+import numpy as np
+import PIL.Image
+import dnnlib
+import dnnlib.tflib as tflib
+import config
+from training import misc
+
+def main(
+    resume_run_id           = 101,     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_snapshot         = None):     # Snapshot index to resume training from, None = autodetect.
+
+    # Initialize TensorFlow.
+    tflib.init_tf()
+
+    # Load pre-trained network.
+    network_pkl = misc.locate_network_pkl(resume_run_id, resume_snapshot)
+    print('Loading networks from "%s"...' % network_pkl)
+    G, D, Gs = misc.load_pkl(network_pkl)
+
+    # Print network details.
+    Gs.print_layers()
+
+    # Pick latent vector.
+    rnd = np.random.RandomState(5)
+    latents = rnd.randn(1, Gs.input_shape[1])
+
+    # Generate image.
+    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+
+    # Save image.
+    os.makedirs(config.result_dir, exist_ok=True)
+    png_filename = os.path.join(config.result_dir, 'example.png')
+    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+
+if __name__ == "__main__":
+    main()
