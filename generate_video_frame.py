334b7810401bd95c4bd5cacf5804ba72d56f603c
yuma <fableooon@gmail.com>
2019-03-10 00:11:06 +0900

    add new func

--- generate_video_frame.py
+++ generate_video_frame.py
@@ -17,32 +17,61 @@ import config
 from training import misc
 
 def main(
-    resume_run_id           = 101,     # Run ID or network pkl to resume training from, None = start from scratch.
-    resume_snapshot         = None):     # Snapshot index to resume training from, None = autodetect.
+    run_id          = 101,     # Run ID or network pkl to resume training from, None = start from scratch.
+    snapshot        = None、     # Snapshot index to resume training from, None = autodetect.
+    grid_size       = [1,1],
+    image_shrink    = 1,
+    image_zoom      = 1,
+    duration_sec    = 60.0,
+    smoothing_sec   = 1.0,
+    mp4             = None,
+    mp4_fps         = 30,
+    mp4_codec       = 'libx265',
+    mp4_bitrate     = '16M',
+    random_seed     = 1000,
+    minibatch_size  = 8):
 
     # Initialize TensorFlow.
     tflib.init_tf()
 
     # Load pre-trained network.
-    network_pkl = misc.locate_network_pkl(resume_run_id, resume_snapshot)
+    network_pkl = misc.locate_network_pkl(run_id, snapshot)
     print('Loading networks from "%s"...' % network_pkl)
     G, D, Gs = misc.load_pkl(network_pkl)
+
+    mp4 = '%s-lerp.mp4' % network_pkl
+    num_frames = int(np.rint(duration_sec * mp4_fps))
 
     # Print network details.
     Gs.print_layers()
 
     # Pick latent vector.
-    rnd = np.random.RandomState(5)
-    latents = rnd.randn(10, Gs.input_shape[1])
+    print('Generating latent vectors...')
+    shape = [num_frames, np.prod(grid_size)] + Gs.input_shape[1:] # [frame, image, channel, component]
+    all_latents = random_state.randn(*shape, Gs.input_shape[1]).astype(np.float32)
+    all_latents = scipy.ndimage.gaussian_filter(all_latents, [smoothing_sec * mp4_fps] + [0] * len(Gs.input_shape), mode='wrap')
+    all_latents /= np.sqrt(np.mean(np.square(all_latents)))
 
-    # Generate image.
+    # Frame generation func for moviepy.
     fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+    def make_frame(t):
+        frame_idx = int(np.clip(np.round(t * mp4_fps), 0, num_frames - 1))
+        latents = all_latents[frame_idx]
+        labels = np.zeros([latents.shape[0], 0], np.float32)
+        # images = Gs.run(latents, labels, minibatch_size=minibatch_size, num_gpus=config.num_gpus, out_mul=127.5, out_add=127.5, out_shrink=image_shrink, out_dtype=np.uint8)
+        images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+        grid = misc.create_image_grid(images, grid_size).transpose(1, 2, 0) # HWC
+        if image_zoom > 1:
+            grid = scipy.ndimage.zoom(grid, [image_zoom, image_zoom, 1], order=0)
+        if grid.shape[2] == 1:
+            grid = grid.repeat(3, 2) # grayscale => RGB
+        return grid
 
-    # Save image.
-    os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    # Generate video.
+    import moviepy.editor # pip install moviepy
+    result_subdir = config.result_dir
+    moviepy.editor.VideoClip(make_frame, duration=duration_sec).write_videofile(os.path.join(result_subdir, mp4), fps=mp4_fps, codec='libx264', bitrate=mp4_bitrate)
+    open(os.path.join(result_subdir, '_done.txt'), 'wt').close()
 
 if __name__ == "__main__":
     main()
