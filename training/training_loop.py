f3a7db67e076794fb3d502a46374036ad673651c
yuma <fableooon@gmail.com>
2019-03-09 18:23:17 +0900

    cond

--- training/training_loop.py
+++ training/training_loop.py
@@ -135,7 +135,7 @@ def training_loop(
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
     resume_run_id           = 100,     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
-    resume_kimg             = 9000,      # Assumed training progress at the beginning. Affects reporting and training schedule.
+    resume_kimg             = 10000,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
 
     # Initialize dnnlib and TensorFlow.
